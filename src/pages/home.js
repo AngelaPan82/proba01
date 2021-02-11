98671f9e1f981126a6a239b554992cd644fa558a
AngelaPan82 <angelapanovska82@gmail.com>
2021-02-11 05:56:14 +0100

    comit

---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export const Home = () => {
+
+    return (
+        <div>
+            <h2>Home</h2>
+            <div>
+        
+            </div>
+    </div>
+    )
+}
